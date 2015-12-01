1120a420875b80dd6811cef68ee74768bad22109
Lea Rozen Sudry <lrozensudry@gmail.com>
2015-12-01 15:49:50 +0100

    Architecture dossier

---
+++ GLImac-Template-build/glimac/CMakeFiles/glimac.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Cone.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Cone.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Geometry.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Geometry.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Image.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Image.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Program.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Program.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/SDLWindowManager.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/SDLWindowManager.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Shader.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Shader.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/Sphere.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/Sphere.cpp.o"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/src/tiny_obj_loader.cpp" "/home/lea/Documents/OpenGLIMAC/GLImac-Template-build/glimac/CMakeFiles/glimac.dir/src/tiny_obj_loader.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/glimac/include"
+  "/home/lea/Documents/OpenGLIMAC/GLImac-Template/third-party/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
