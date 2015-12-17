1b867253722efa88de8df6bf4a0e7878646f8587
JulienRst <julien.rousset01@gmail.com>
2015-12-17 11:58:10 +0100

    ADD _T_ASSIMP (test of assimp with the tutorial)

---
+++ GLImac-Template/_T_ASSIMP/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "mesh.hpp"
+#include "model.hpp"
+#include "shader.hpp"
+
+int main(){
+    cout << "ça compile et ça fonctionne" << endl;
+    return 0;
+}
