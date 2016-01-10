6329d5a4d7ed12d4d03b00de406380f7f72934b5
JulienRst <julien.rousset01@gmail.com>
2016-01-10 01:02:06 +0100

    [CLEAN] CLEAN AND COMMENT THE MAIN.CPP
    --> Change some little things to be more clear

--- files/game/main.cpp
+++ files/game/main.cpp
@@ -1,30 +1,18 @@
+// --- Include STD & IOSTREAM --- //
 #include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <vector>
-#include <map>
-#include <set>
-#include <memory>
-#include <string>
-#include <vector>
-
-#include <glimac/glm.hpp>
+// --- Include Needed From GLImac --- //
 #include <glimac/SDLWindowManager.hpp>
-#include <glimac/Sphere.hpp>
-
+#include <glimac/FilePath.hpp>
+// --- Include Glew --- //
 #include <GL/glew.h>
-
+// --- Include Needed From Engine --- //
 #include "engine/model.hpp"
 #include "engine/mesh.hpp"
 #include "engine/shader.hpp"
 #include "engine/freefly.hpp"
 #include "engine/mouse.hpp"
-// #include "engine/music.hpp"
-#include "engine/music.hpp" //Normalement c'est lui qui accède correctement au fichier
-
-#include <SDL/SDL.h>
-#include <SDL/SDL_mixer.h>
+#include "engine/music.hpp"
 
 // -------- NAMESPACE -------------- //
 
@@ -32,194 +20,191 @@ using namespace glm;
 using namespace std;
 using namespace glimac;
 
-// TODO Ajouter ici l'initialisation de la caméra de Chamse
+// -------- MAIN PROGRAM -------------- //
+
 
 int main(int argc, char** argv){
 
-    FilePath app = FilePath(argv[0]).dirPath() + "../";
-    // -------- GLOBAL VARIABLE -------------- //
+        // -------------------------------------------- //
+        // -------------- GLOBAL VARIABLE ------------- //
+        // -------------------------------------------- //
 
+    //Windth and Height of the app
     GLuint screenWidth = 1920;
     GLuint screenHeight = 1080;
-
-
-    //Initialisation de la fenêtre
-    SDLWindowManager windowManager(screenWidth, screenHeight, "TEST ASSIMP");
-
-    //TEST SUR GLEW
+    //Define the path of the main app
+    FilePath app = FilePath(argv[0]).dirPath() + "../";
+    //Initialize Window
+    SDLWindowManager windowManager(screenWidth, screenHeight, "Lost Town");
+    //Check if Glew is ok
     GLenum glewInitError = glewInit();
     if(GLEW_OK != glewInitError) {
         std::cerr << glewGetErrorString(glewInitError) << std::endl;
         return EXIT_FAILURE;
     }
 
-    //Affichage des versions Software
-    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
-
-    // Initialisation de GLEW
+    // Initialize Glew
     glewExperimental = GL_TRUE;
     glewInit();
     glViewport(0, 0, screenWidth, screenHeight);
     glEnable(GL_DEPTH_TEST);
 
-    // Initialisation des models
-
+    // Initialize Models
     map<int, unique_ptr<Model> > models = modelsFromFile(app + FilePath("assets/models/models.txt"));
 
-    // Initialisation de la Caméra freefly
-
+    // Initialize Camera FreeFly & it's viewMatrix
     Camera camera = Camera();
     mat4 viewMatrix;
 
-    // Initialisation de l'objet Mouse
-
+    // Initialize Mouse & Offset
     Mouse mouse;
-
-    //Load & Compile Shader
-
-    Shader shader(app + "shaders/model_loading.vs",app + "shaders/model_loading.frag");
-
-
-    int loop = true;
     float xOffset, yOffset;
 
+    //Load & Compile Shader
+    Shader shader(app + "shaders/model_loading.vs",app + "shaders/model_loading.frag");
 
-    // *************************************
-    // SOUNDS
-    // *************************************
+        // -------------------------------------------- //
+        // -------------- SOUND VARIABLE -------------- //
+        // -------------------------------------------- //
 
+    //Initialize SDL_Mixer and SDL_Audio
     SDL_Init(SDL_INIT_AUDIO);
     InitAudio();
+    AdjustChannelVolume(-1, MIX_MAX_VOLUME/5);
 
-    //Vector how put sounds (music and chunk)
+    //Vector to put sounds (music and chunk)
     vector<Mix_Music*> musicList;
     vector<Mix_Chunk*> chunkList;
 
+        // ------- GENERAL MUSIQUE ----- //
 
-
-    // ------------------- MENU MANAGE ----------------------
-
-    //Music Menu
+    //Music for the Menu
     musicList.push_back(LoadMusic((app + "assets/sounds/bruit_menu.mp3").c_str()));
     PlayMusic(musicList[0], -1); // -1 to load at infinity
-    //Sound Menu
-    chunkList.push_back(LoadSound((app + "assets/sounds/footstep_1pas.ogg").c_str()));
-    AdjustChannelVolume(-1, MIX_MAX_VOLUME/10);//Dicrease the music volume with '/10'
-    // print the average volume
-    //printf("Average volume is %d\n",Mix_Volume(-1,-1));
-    Uint32 lastFootStep = 0;
-    Uint32 limit = 600;
-    while(loop){
-        // Event loop:
-        SDL_Event e;
 
+    /* --- TODO : use :
+        if(windowManager.isKeyPressed(SDLK_#)){
+            StopMusic(); //Met la musique en pause
+        }
+        if(windowManager.isKeyPressed(SDLK_#)){
+            ResumeMusic(); //Relance la musique de là où elle s'est arrêté
+        }
+        if(windowManager.isKeyPressed(SDLK_#)){
+            Mix_RewindMusic(); //Revient au début de la musique
+        }
+        if(windowManager.isKeyPressed(SDLK_#)){
+            Mix_HaltMusic(); //Arrête la musique
+        }
+    --- */
+
+        // ------- CONTEXTUAL NOISE ----- //
+
+    //Foot Steps
+    chunkList.push_back(LoadSound((app + "assets/sounds/footstep_1pas.ogg").c_str()));
+    Uint32 lastFootStep = 0;
+    Uint32 limitBetweenFootStep = 600; // ms min between two foot step sound
+
+        // -------------------------------------------- //
+        // ----------- LOOP OF THE PROGRAM ------------ //
+        // -------------------------------------------- //
+
+    int loop = true;
+    while(loop){
+
+        // ---------------------------- CHECK IF SDL QUIT
+        SDL_Event e;
         while(windowManager.pollEvent(e)) {
             if(e.type == SDL_QUIT) {
                 loop = false; // Leave the loop after this iteration
             }
         }
 
-        //glClear
+        // ---------------------------- GL CLEAR
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f),
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        //Lancement des shaders
-        shader.Use();
-        // ---------------------------- RECUPERATION DES EVENTS CLAVIER / UPDATE CAMERA
 
+        // ---------------------------- LAUNCH OF SHADDER
+        shader.Use();
+
+        // ---------------------------- GET MOUSE
         xOffset = windowManager.getMousePosition().x - mouse.lastX;
         yOffset = windowManager.getMousePosition().y - mouse.lastY;
-
         mouse.lastX = windowManager.getMousePosition().x;
         mouse.lastY = windowManager.getMousePosition().y;
 
-
+        // ---------------------------- UPDATE CAMERA
         camera.ProcessMouseMovement(xOffset,yOffset);
+
+        // ---------------------------- PROCESS JUMP
         camera.ProcessJump();
 
+            // ---------------------------------------------
+            // ---------------------------- PROCESS KEYBOARD
+            // ---------------------------------------------
+        // ---------------------------- Initalize movement to FALSE
+        bool isMovement = false;
+        // ---------------------------- Z : Forward
         if(windowManager.isKeyPressed(SDLK_z)){
             camera.MoveFront(0.010);
-            if(SDL_GetTicks() - lastFootStep > limit){
-                PlaySound(chunkList[0]);
-                lastFootStep = SDL_GetTicks();
-            }
+            isMovement = true;
         }
-
+        // ---------------------------- Q : Left
         if(windowManager.isKeyPressed(SDLK_q)){
             camera.MoveRight(-0.010);
-            if(SDL_GetTicks() - lastFootStep > limit){
-                PlaySound(chunkList[0]);
-                lastFootStep = SDL_GetTicks();
-            }
+            isMovement = true;
         }
+        // ---------------------------- S : Backward
         if(windowManager.isKeyPressed(SDLK_s)){
             camera.MoveFront(-0.010);
-            if(SDL_GetTicks() - lastFootStep > limit){
-                PlaySound(chunkList[0]);
-                lastFootStep = SDL_GetTicks();
-            }
+            isMovement = true;
         }
+        // ---------------------------- D : Right
         if(windowManager.isKeyPressed(SDLK_d)){
             camera.MoveRight(0.010);
-            if(SDL_GetTicks() - lastFootStep > limit){
-                PlaySound(chunkList[0]);
-                lastFootStep = SDL_GetTicks();
-            }
+            isMovement = true;
         }
-
-        if(windowManager.isKeyPressed(SDLK_RETURN)){
-            models = modelsFromFile(app + FilePath("files/assets/models/models2.txt"));
-        }
-        // if(windowManager.isKeyPressed(SDLK_SPACE))
-        //     camera.MoveUp(0.010);
-        // if(windowManager.isKeyPressed(SDLK_LSHIFT))
-        //     camera.MoveUp(-0.010);
+        // ---------------------------- SPACE : Jump
         if(windowManager.isKeyPressed(SDLK_SPACE))
             camera.launchJump();
+
+        if(isMovement && SDL_GetTicks() - lastFootStep > limitBetweenFootStep){
+            PlaySound(chunkList[0]);
+            lastFootStep = SDL_GetTicks();
+        }
+        // ---------------------------- SHIFT : Run
         if(windowManager.isKeyPressed(SDLK_LSHIFT)){
-            limit = 400;
+            limitBetweenFootStep = 400;
             camera.isShiftPressed = true;
         } else {
-            limit = 600;
+            limitBetweenFootStep = 600;
             camera.isShiftPressed = false;
         }
-
-        // -------------------- EVENT KEYBOARD MUSICS ---------------------
-
-        if(windowManager.isKeyPressed(SDLK_p)){
-            StopMusic();
-        }
-        if(windowManager.isKeyPressed(SDLK_o)){
-            ResumeMusic();
-        }
-        if(windowManager.isKeyPressed(SDLK_BACKSPACE)){
-            Mix_RewindMusic(); //Revient au début de la musique
-        }
-        if(windowManager.isKeyPressed(SDLK_ESCAPE)){
-            Mix_HaltMusic(); //Arrête la musique
+        // ----------------------------- ENTER : Go to next level : TODO : REMOVE IT !!
+        if(windowManager.isKeyPressed(SDLK_RETURN)){
+            models = modelsFromFile(app + FilePath("assets/models/models2.txt"));
         }
 
 
-        // RECUPERATION DE LA SOURIS / UPDATE CAMERA
+            // ---------------------------------------------
+            // --------------------------------- SEND MATRIX
+            // ---------------------------------------------
 
-        // ---------------------------- FIN RECUPERATION EVENTS
-
-
-        //ENVOIE DES MATRICES
-
-        //Récupération de la viewMatrix de la Caméra
-
+        // ---------------------------- GET THE VIEW MATRIX FROM CAMERA
         viewMatrix = camera.GetViewMatrix();
-
-        // Transformation matrices
+        // ---------------------------- TRANSFORM THE MATRIX AND SEND THEMP
         glm::mat4 projection = glm::perspective(70.0f, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
+        // ---------------------------- CALLING THE DRAW METHOD OF ALL THE MODELS
         drawModels(models, shader);
-
-        //Update the display
+        // ---------------------------- SWAP THE BUFFERS
         windowManager.swapBuffers();
     }
+
+    // --------------------------------------------- //
+    // ---------- FREE AND LEAVE PROPERLY ---------- //
+    // --------------------------------------------- //
+
     FreeSound(chunkList[0]);
     FreeMusic(musicList[0]);
     QuitAudio();
